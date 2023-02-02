f95a829ce4bb92867702f97e1aea9578c844b675
mingi <meengi07@gmail.com>
2023-02-02 23:22:00 +0900

    fix : study tutorial code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,26 +5,32 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+
+public class App {
+    public static void main( String[] args ) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("A");
-        em.persist(member);
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloWorld");
+            em.persist(member);
 
-        em.close();
+            Member findMem = em.find(Member.class, 101L);
+            Member findMem2 = em.find(Member.class, 101L);
+            System.out.println("find member : " + findMem.getName());
+            System.out.println("find member : " + findMem.getName());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
